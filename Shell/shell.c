0b538131a866e6c47bc32a8ae3ef3ac455fb9c83
dsaf2007 <dsaf2007@naver.com>
2021-04-25 02:15:49 +0900

    exec

--- Shell/shell.c
+++ Shell/shell.c
@@ -6,7 +6,7 @@
 #include <fcntl.h>
 
 #define MAX_HIST 40
-
+#define MAX_LINE 80
 
 typedef char* string;
 string* history[MAX_HIST];
@@ -16,14 +16,105 @@ void dispHistory();
 void addHistory(string hist);
 int main()
 {
-	string text1=NULL, text2=NULL;
-	history_index=-1;
+	string text1="exit\n", text2=NULL;
+	history_index=0;
+	
+	while(1)
+	{
+	//	char input[MAX_LINE/2+1];
+		string input;
+		input = (char*)malloc(sizeof(char)*(MAX_LINE/2 +1));
+		fflush(stdout);
+		read(0,input,MAX_LINE/2+1);
+
+		if(strcmp(text1,input)==0)
+		{
+			printf("exit shell");
+			break;
+		}
+		if(input[0]=='!')
+		{
+			if(history_index == 0)
+			{
+				printf("No Command in history\n");
+				continue;
+			}
+			else if(input[1] == '!')
+			{
+				strcpy(input,history[history_index]);
+			}
+			else
+			{
+				int num = (int)(input[1]-'0');
+				if(num>history_index)
+				{
+					printf("number out of index\n");continue;
+				}
+				else if(num<=history_index)
+				{
+					strcpy(input,history[num]);
+				}
+				else
+				{
+					printf("No such command in history\n");
+				}
+			}	
+		}
+
+		switch(fork())
+		{
+			case -1:
+				perror("fork error");
+				break;
+			case 0:
+				input[strlen(input)]='\0';
+				printf("%s",input);
+				addHistory(input);
+				if(strcmp(input,"history\0")==0)
+				{
+					printf("s");
+					dispHistory();
+					exit(0);
+				}
+				else
+				{
+
+					text1=strtok(input," ");
+					text2=strtok(NULL,"\0");
+					char* args={"ls","-al"};
+					execvp(args[0],args);
+					exit(0);
+				}
+				wait(NULL);
+
+
+		}
+	}
+	return 0;
 }
 
 
 void addHistory(string hist)
 {
 	history_index++;
-	history[history_index]=&hist;
-	//trcpy(history[history_index],hist);
+	history[history_index]=(char**)malloc(sizeof(char)*strlen(hist));
+	strcpy(history[history_index],hist);
+}
+
+void dispHistory()
+{
+	if(history_index<11)
+	{
+		for(int i = history_index;i>0;i++)
+		{
+			printf("%d %s\n",i,history[i]);
+		}
+	}
+	else
+	{
+		for(int i = history_index;i>history_index-10;i--)
+		{
+			printf("%d %s\n",i,history[i]);
+		}
+	}
 }
