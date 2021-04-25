0fb4f2a04d7f729b497fd403e1327a11a931b121
dsaf2007 <dsaf2007@naver.com>
2021-04-26 03:57:08 +0900

    hist

--- Shell/shell_history.c
+++ Shell/shell_history.c
@@ -81,7 +81,7 @@ int main()
 			strcat(front,"\0");
 			strcat(back,"\0");
 			argv = setArgs(front);
-			//argv2 = setArgs(back);
+			argv2 = setArgs(back);
 			if(pipe(fd)==-1)//파이프 생성
 			{
 				printf("fail to call pipe()\n");
